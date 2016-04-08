950e2762cbb618fc432c08cbecfd710e069ce59e
sandyso <sandyrasom@gmail.com>
2016-04-08 09:39:06 -0400

    updated js

--- js/effects.js
+++ js/effects.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-$("#blue").hover(function() {
+$(".hcolor").toggle(display) {
+if (display === '#blue') {
+	$("h1").show("#0000ff")
+} else if (display === '#green') {
+	$("h1").show("#0066400")
+}
+
+/*$("#blue").hover(function() {
+	$(".header").toggle()
 	});
 $("#green").hover(function() {
 console.log("it works!")	
-});
+});*/
 });
